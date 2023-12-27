b30198756c3e21d611e3cadbd77452924ee0e197
DragCk <146335758+DragCk@users.noreply.github.com>
2023-12-28 00:51:52 +0800

    Add about

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,30 @@
+import { HiArrowNarrowRight } from "react-icons/hi";
+
 const Home = () => {
   return (
-    <div
-      className="w-full
-   h-screen bg-[#08192F]"
-    ></div>
+    <div name="home" className="w-full h-screen bg-[#08192F]">
+      {/* ====== Container ======*/}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi, my name is </p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Julio Tsai
+        </h1>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit
+          distinctio qui rem. Maiores, consectetur. Ad consequatur fugit ea enim
+          suscipit delectus repellendus, quam itaque debitis quas sequi eveniet,
+          non sapiente nostrum similique! Maiores, corporis atque ab deserunt
+          numquam repellat dolore officia. Dolor aliquid, praesentium aut
+          possimus blanditiis incidunt ab temporibus.
+        </p>
+        <div>
+          <button className="group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View Work
+            <HiArrowNarrowRight className="ml-4 group-hover:rotate-90 duration-300" />
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/SocialIcon.jsx
+++ src/components/SocialIcon.jsx
@@ -1,6 +1,6 @@
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const SocialIcon = () => {
   return (
@@ -14,7 +14,7 @@ const SocialIcon = () => {
             Github <FaGithub size={30} />
           </a>
         </li>
-        <li className="w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-300">
+        <li className="w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500">
           <a
             className="flex justify-between items-center w-full text-gray-300"
             href="/"
@@ -22,12 +22,20 @@ const SocialIcon = () => {
             Email <HiOutlineMail size={30} />
           </a>
         </li>
-        <li className="w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-green-500">
+        <li className="w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-green-700">
           <a
             className="flex justify-between items-center w-full text-gray-300"
             href="/"
           >
             Resume <BsFillPersonLinesFill size={30} />
+          </a>
+        </li>
+        <li className="w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500">
+          <a
+            className="flex justify-between items-center w-full text-gray-300"
+            href="/"
+          >
+            LinkedIn <FaLinkedin size={30} />
           </a>
         </li>
       </ui>
